a4dd48e967290a2b77e98b2247eee70768d547c0
wahibmuhibi <147083627+wahibmuhibi@users.noreply.github.com>
2024-06-26 00:37:16 +0700

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Stop and remove any existing containers
+# Stop and remove any existing container with the same name
 sudo docker stop ec2-flask || true
 sudo docker rm ec2-flask || true
 
 # Change directory to where Dockerfile is located
 cd /home/ec2-user/downloads
 
-# Build the Docker image (if needed)
-# This step may not be necessary if you're pushing the image directly from GitHub Actions
-# sudo docker build -t ec2-flask:v1.0 -f Dockerfile .
+# Build the Docker image
+sudo docker build -t ec2-flask:${GITHUB_SHA} .
 
-# Run the Docker image
-sudo docker run -d -p 80:5000 --name ec2-flask:v1.0 ec2-flask:v1.0
+# Run the Docker container
+sudo docker run -d -p 5000:5000 --name ec2-flask ec2-flask:${GITHUB_SHA}
