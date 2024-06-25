da15ab90908c6d2950edbe2d26957aae3743bc4c
wahibmuhibi <147083627+wahibmuhibi@users.noreply.github.com>
2024-06-26 00:41:06 +0700

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -5,7 +5,10 @@ sudo docker stop ec2-flask || true
 sudo docker rm ec2-flask || true
 
 # Change directory to where Dockerfile is located
-cd /home/ec2-user/downloads
+cd /home/ec2-user/downloads  # Adjust this path as per your setup
+
+# Pull the latest changes from GitHub (optional)
+git pull origin main
 
 # Build the Docker image
 sudo docker build -t ec2-flask:${GITHUB_SHA} .
